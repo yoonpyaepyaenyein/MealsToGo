a91f58567385b8257a53d8aa2311fe2aac7fcac4
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-05-21 22:13:14 +0630

    Restaurant-Info Card Using React-Native-Paper

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,12 +1,36 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Card } from "react-native-paper";
 
-export const RestaurantInfo = () => {
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
   return (
-    <View>
-      <Text>Restaurant Info</Text>
-    </View>
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Text style={styles.title}>{name}</Text>
+    </Card>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+  },
+  cover: {
+    padding: 20,
+    backgroundColor: "white",
+  },
+  title: {
+    padding: 16,
+  },
+});
