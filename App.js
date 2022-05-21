83866cd5281249f3c4d15f22071d33a592628dc9
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-05-21 12:37:22 +0630

    StatusBar on Android using PlatForm and SafeAreaView

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView, Text, Platform, View } from "react-native";
+
+const isAndroid = Platform.OS === "android";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: isAndroid ? 50 : 0 }}>
+        <View style={{ padding: 16, backgroundColor: "green" }}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
