ea504fe2798179b9bce8b15668556cc41f9bdaed
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-05-24 12:01:19 +0630

    Creating A Typography Component and CleanUp

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,4 +1,4 @@
-import { Text, View, Image } from "react-native";
+import { Image } from "react-native";
 import React from "react";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
@@ -8,6 +8,8 @@ import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
+import { Icon } from "./restaurant-info-card.styles";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -21,12 +23,6 @@ const RestaurantCardCover = styled(Card.Cover)`
 const Address = styled(Text)`
   font-family: ${(props) => props.theme.fonts.body};
   font-size: ${(props) => props.theme.fontSizes.caption};
-`;
-
-const Title = styled.Text`
-  font-family:${(props) => props.theme.fonts.heading}
-  font-size:${(props) => props.theme.fontSizes.body}
-  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 const Info = styled.View`
@@ -73,7 +69,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
-        <Title>{name}</Title>
+        <Text variant="label">{name}</Text>
         <Section>
           <Rating>
             {ratingArray.map(() => (
@@ -82,9 +78,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
           </Rating>
           <SectionEnd>
             {isClosedTemporarily && (
-              <Text variant="label" style={{ color: "red" }}>
-                CLOSED TEMPORARILY
-              </Text>
+              <Text variant="error">CLOSED TEMPORARILY</Text>
             )}
             <Spacer position="left" size="large">
               {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
---
+++ src/features/restaurants/components/restaurant-info-card.styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components/native";
+
+export const Icon = styled.Image`
+  width: 15px;
+  height: 15px;
+`;
