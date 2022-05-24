7a75057c290a039418cefe4b56df8e1861c91410
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-05-24 13:29:46 +0630

    No Inline Styles

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -16,9 +16,11 @@ const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-// const RestaurantList = styled.FlatList`
-//   padding: ${(props) => props.theme.space[3]};
-// `;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
 export const RestaurantsScreen = () => {
   return (
