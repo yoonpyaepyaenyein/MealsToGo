a3022620922093942843fca742f3a02332b0d497
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-05-25 11:53:32 +0630

    Navigation Installation & Bottom tab Navigation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
+import { Text } from "react-native";
 
 import {
   useFonts as useOswald,
@@ -8,9 +9,19 @@ import {
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
+//Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 // COMPONENTS
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { theme } from "./src/infrastructure/theme";
+
+const Tab = createBottomTabNavigator();
+
+const Setting = () => <Text>Settings</Text>;
+const Map = () => <Text>Map</Text>;
+// const Restaurant = () => <Text>Restaurant</Text>;
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -28,7 +39,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurant" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={Map} />
+            <Tab.Screen name="Setting" component={Setting} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
