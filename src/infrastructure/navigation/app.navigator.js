28fc59b02de92f6891b2fa09527ac80fbe2ffa8d
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-05-29 22:14:49 +0630

    Installing React Native Map

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -8,6 +8,8 @@ import { SafeArea } from "../../components/utility/safe-area.component";
 
 import { RestaurantsNavigator } from "./restaurants.navigator";
 
+import { MapScreen } from "../../features/map/screen/map.screen";
+
 const Tab = createBottomTabNavigator();
 
 const TAB_ICON = {
@@ -19,12 +21,6 @@ const TAB_ICON = {
 const Settings = () => (
   <SafeArea>
     <Text>Settings</Text>
-  </SafeArea>
-);
-
-const Map = () => (
-  <SafeArea>
-    <Text>Map</Text>
   </SafeArea>
 );
 
@@ -47,7 +43,7 @@ export const AppNavigator = () => (
       }}
     >
       <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-      <Tab.Screen name="Map" component={Map} />
+      <Tab.Screen name="Map" component={MapScreen} />
       <Tab.Screen name="Settings" component={Settings} />
     </Tab.Navigator>
   </NavigationContainer>
