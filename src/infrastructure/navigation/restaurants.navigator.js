3d9767a8908822eb9a5ee2cd8e42e9b696f3bc00
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-05-29 20:43:48 +0630

    Solution Rendering The Card

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { Text } from "react-native";
+
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { TransitionPresets } from "@react-navigation/stack";
+
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
+import { RestaurantDetailScreen } from "../../features/restaurants/screens/restaurant-detail.screen";
 
 const RestaurantStack = createNativeStackNavigator();
 
@@ -20,7 +22,7 @@ export const RestaurantsNavigator = () => {
       />
       <RestaurantStack.Screen
         name="RestaurantDetail"
-        component={() => <Text>Restaurant Detail</Text>}
+        component={RestaurantDetailScreen}
       />
     </RestaurantStack.Navigator>
   );
