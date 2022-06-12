a7bd478b254c608a160383c2911b2dc31cd51d9b
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-06-12 12:10:58 +0630

    Lighten Up The BAckground

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { AccountBackground } from "../components/account.styles";
+import { AccountCover } from "../components/account.styles";
 
 export const AccountScreen = () => {
-  return <AccountBackground />;
+  return (
+    <AccountBackground>
+      <AccountCover />
+    </AccountBackground>
+  );
 };
