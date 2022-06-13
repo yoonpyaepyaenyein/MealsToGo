963f5bbacbe3c1fcc56751fb60b5d682c9cef01f
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-06-13 19:28:10 +0630

    Making The Setting Screen Look Nice

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -10,6 +10,15 @@ export const AuthenticationContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
+  firebase.auth().onAuthStateChanged((usr) => {
+    if (usr) {
+      setUser(usr);
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);
+    }
+  });
+
   const onLogin = (email, password) => {
     setIsLoading(true);
     loginRequest(email, password)
