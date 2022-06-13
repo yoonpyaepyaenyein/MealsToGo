72dfd64a657e35abde04b13ce47467b536335059
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-06-13 15:15:07 +0630

    Feature Overview:Registration

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -23,6 +23,23 @@ export const AuthenticationContextProvider = ({ children }) => {
       });
   };
 
+  const onRegister = (email, password, repeatedPassword) => {
+    if (password !== repeatedPassword) {
+      setError("Error: Password do not match");
+    }
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((u) => {
+        setUser(u);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setError(e.toString());
+      });
+  };
+
   return (
     <AuthenticationContext.Provider
       value={{
@@ -31,6 +48,7 @@ export const AuthenticationContextProvider = ({ children }) => {
         isLoading,
         error,
         onLogin,
+        onRegister,
       }}
     >
       {children}
