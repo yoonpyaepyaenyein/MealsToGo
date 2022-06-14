5a9798041feb06a358eac9224e6cc239b8d0cd64
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-06-14 11:46:53 +0630

    Solution Expo Camera

--- src/features/setting/screens/camera.screen.js
+++ src/features/setting/screens/camera.screen.js
@@ -1,3 +1,39 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { View } from "react-native";
+import styled from "styled-components/native";
 
-export const CameraScreen = () => null;
+import { Camera } from "expo-camera";
+
+import { Text } from "../../../components/typography/text.component";
+
+const ProfileCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
+`;
+
+export const CameraScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+
+  const cameraRef = useRef();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <ProfileCamera
+      ref={(camera) => (cameraRef.current = camera)}
+      type={Camera.Constants.Type.front}
+    ></ProfileCamera>
+  );
+};
